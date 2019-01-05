c1571262fb0059ce4619fdc1c4cb5cb478edee5f
daglus <daglus.ck.ua@gmail.com>
2019-01-05 22:27:41 +0200

    Add display multiple error messages. Finish Lesson 24.

--- Lesson 24/app.rb
+++ Lesson 24/app.rb
@@ -25,11 +25,10 @@ post '/visit' do
          :phone => 'Введите телефон',
          :datetime => 'Введите дату визита' }
 
-  hh.each do |key, value|
-    if params[key] == ''
-      @error = hh[key]
-      return erb :visit
-    end
+  @error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+  if @error != ''
+    return erb :visit
   end
 
 	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
